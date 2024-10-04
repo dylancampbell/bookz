c9cc18788ec3f8a8b632159aab4f90947a0a1ee0
dylancampbell <dylancampbell@gmail.com>
2024-10-04 23:01:23 +0000

    Fix urls

--- urls.py
+++ urls.py
@@ -29,15 +29,47 @@ def generate_urls(cleaned_title, cleaned_author):
         st.markdown(f'<a href="{abebooks_url}" target="_blank" class="button abebooks">AbeBooks</a>', unsafe_allow_html=True)
         st.markdown(f'<a href="{bookshop_url}" target="_blank" class="button bookshop">Bookshop.org</a>', unsafe_allow_html=True)
 
-# Individual URL generation functions
+
 def generate_abebooks_url(book_title, author):
     base_url = "https://www.abebooks.com/servlet/SearchResults"
     query_params = {"an": author, "tn": book_title}
     return base_url + "?" + urllib.parse.urlencode(query_params)
 
+def generate_bookshop_url(book_title, author):
+    base_url = "https://bookshop.org/books"
+    query_params = {"keywords": f"{book_title} {author}"}
+    return base_url + "?" + urllib.parse.urlencode(query_params)
+
+def generate_storygraph_url(book_title, author):
+    base_url = "https://app.thestorygraph.com/browse"
+    query_params = {"search_term": f"{book_title} {author}"}
+    return base_url + "?" + urllib.parse.urlencode(query_params)
+
+def generate_goodreads_url(book_title, author):
+    base_url = "https://www.goodreads.com/search"
+    query = f"{book_title} {author}" if book_title and author else book_title or author
+    query_params = {"q": query, "search_type": "books"}
+    return base_url + "?" + urllib.parse.urlencode(query_params)
+
+def generate_amazon_url(book_title, author):
+    base_url = "https://www.amazon.com/s"
+    query = f"{book_title} {author}" if book_title and author else book_title or author
+    query_params = {"k": query}
+    return base_url + "?" + urllib.parse.urlencode(query_params)
+
+def generate_lapl_url(book_title, author):
+    base_url = "https://ls2pac.lapl.org/?section=search"
+    search_data = {
+        "isAnd": True,
+        "searchTokens": [
+            {"searchString": author, "type": "Contains", "field": "Author"} if author else None,
+            {"searchString": book_title, "type": "Contains", "field": "Title"} if book_title else None,
+        ]
+    }
+    search_query = urllib.parse.quote(json.dumps(search_data))
+    return f"{base_url}&term={search_query}&page=0&pageSize=10&sortKey=Relevancy&db=ls2pac"
+
 def generate_libby_url(book_title, author):
     base_url = "https://libbyapp.com/search/lapl/search/query-"
-    search_query = f"{urllib.parse.quote(book_title)}%20{urllib.parse.quote(author)}"
+    search_query = f"{urllib.parse.quote(book_title)}%20{urllib.parse.quote(author)}" if book_title and author else urllib.parse.quote(book_title or author)
     return base_url + search_query + "/page-1"
-
-# Add other URL generation functions similarly...
